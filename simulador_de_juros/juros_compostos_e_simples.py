61e2e9c7a0884100d7dbf390dfa51d4d8782f8df
Gabriel de Albuquerque Vasconcelos <eugabrieldealbuquerque@gmail.com>
2025-01-27 08:14:53 -0300

    adicionando tratamento de erro

--- simulador_de_juros/juros_compostos_e_simples.py
+++ simulador_de_juros/juros_compostos_e_simples.py
@@ -1,20 +1,31 @@
+from sre_constants import error
+
 from funcoes_simulador import *
 # Simulador de investimento que retorna o montante final e o juros simples e composto
 print('-' * 25, 'Simulador de Investimento', '-' * 25)
 continua = 'S'
-while continua == 'S':
-    resposta = menu()
-    if resposta == 1:
-        Capital = capital()
-        Juros = juros()
-        Tempo = tempo()
-        juros_simples(Capital, Juros, Tempo)
-    elif resposta == 2:
-        Capital = capital()
-        Juros = juros()
-        Tempo = tempo()
-        juros_composto(Capital, Juros, Tempo)
-    continua = input('Deseja realizar outra simulação? (S/N) ').upper()
-while continua not in 'SN':
-        continua = input('Erro: opção inválida. Digite S ou N: ').upper()
-print('Obrigado e volte sempre!')
+try:
+    while continua == 'S':
+        resposta = menu()
+        if resposta == 1:
+            Capital = capital()
+            Juros = juros()
+            Tempo = tempo()
+            juros_simples(Capital, Juros, Tempo)
+        elif resposta == 2:
+            Capital = capital()
+            Juros = juros()
+            Tempo = tempo()
+            juros_composto(Capital, Juros, Tempo)
+        else:
+            resposta = int(input('\033[1:31mErro: opção inválida!\033[m Digite 1 ou 2: '))
+        continua = input('Deseja realizar outra simulação? (S/N) ').upper()
+    while continua not in 'SN':
+            continua = input('\033[1:31mErro: opção inválida.\033[m Digite S ou N: ').upper()
+except ValueError:
+    print('Opção inválida! \033[1:31mValor inválido.\033[m')
+except TypeError:
+    print('Opção inválida! \033[1:31mTipo inválido.\033[m')
+finally:
+    print('Obrigado e volte sempre!')
+
